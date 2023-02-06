29b8fe76baae2d7b49cde9e1887b54b800a98eaa
Ruslan Barna <ruslbarna@gmail.com>
2023-02-06 23:57:17 +0200

    real data

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,17 +1,7 @@
-function movies(parent, args) {
-    return {
-        page: 1,
-        totalResults: 10,
-        totalPage: 10,
-        results: [{
-            id: 1,
-            title: 'Movie title',
-            releaseDate: 'release date',
-            posterPath: ''
-        }]
-    }
-}
+const { getPopular } = require('../modules/movies');
 
-module.exports = {
-    movies,
-}
+async function movies(parent, args) {
+
+    const data = await getPopular();
+
+    return data;
